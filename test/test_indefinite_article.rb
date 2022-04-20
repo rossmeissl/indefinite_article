41eb976ffccd4dd93d1b7a8c4c819d89b273da95
glacials <ben@twos.dev>
2022-04-19 19:25:52 -0700

    Assume initialisms aren't pronounceable by default

--- test/test_indefinite_article.rb
+++ test/test_indefinite_article.rb
@@ -2,7 +2,9 @@ require File.expand_path('../helper', __FILE__)
 
 class TestIndefiniteArticle < Minitest::Test
   AN_WORDS = %w{
+    AARP
     apple
+    ASAP
     unassailable
     ubuntu
     ubersexual
@@ -13,18 +15,26 @@ class TestIndefiniteArticle < Minitest::Test
     unlikely
     honor
     honorable
+    HRDC
+    IP
     onerous
+    OSHA
     hour
     honest
     heir
     NDA
+    NPC
     utter
     urgent
+    XL
+    XXL
     a e f h i l m n o r s x
     8 11
   }
 
   A_WORDS = %w{
+    3M
+    BBS
     ukulele
     UN
     uk
@@ -38,6 +48,13 @@ class TestIndefiniteArticle < Minitest::Test
     ouija
     european
     ewe
+    FEMA
+    LASER
+    NAFTA
+    NATO
+    PIN
+    SCUBA
+    SWAT
     ubiquity
     uboat
     unicorn
@@ -47,6 +64,7 @@ class TestIndefiniteArticle < Minitest::Test
     useful
     urinologist
     urea
+    VIP
     b c d g j k p q t u v w y z
     1 2 3 4 5 6 7 9 10 12 13 14 15 16 17 18 19 20
   }
@@ -61,14 +79,12 @@ class TestIndefiniteArticle < Minitest::Test
   def test_indefinite_article_selection_for_an
     AN_WORDS.each do |word|
       assert_equal "an", word.indefinite_article, "word: #{word}"
-      assert_equal "an", word.upcase.indefinite_article, "word: #{word.upcase}"
     end
   end
 
   def test_indefinite_article_selection_for_a
     A_WORDS.each do |word|
       assert_equal "a", word.indefinite_article, "word: #{word}"
-      assert_equal "a", word.upcase.indefinite_article, "word: #{word.upcase}"
     end
   end
 
